cd0beb8693a6cb9e64f251557e9dd976b3bface8
dad-siberian <dad.siberian@gmail.com>
2024-03-17 20:29:57 +0300

    added django-tinymce for the description_long field of the Place model

--- places/models.py
+++ places/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField(max_length=200, blank=True)
     description_short = models.TextField()
-    description_long = models.TextField()
+    description_long = HTMLField()
     lng = models.FloatField()
     lat = models.FloatField()
 
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from where_to_go import views
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("", views.show_main),
     path("admin/", admin.site.urls),
     path("places/<int:place_id>/", views.show_place_detail),
+    path("tinymce/", include("tinymce.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
