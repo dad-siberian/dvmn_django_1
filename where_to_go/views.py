453482924d53d9142e1bde070d7fd7c0b674ee89
dad-siberian <dad.siberian@gmail.com>
2024-03-17 18:55:44 +0300

    Move data from template to view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,5 +1,32 @@
+import json
+
 from django.shortcuts import render
 
 
 def show_main(request):
-    return render(request, "index.html")
+    files = {
+        "roofs24": "static/places/roofs24.json",
+        "moscow_legends": "static/places/moscow_legends.json",
+    }
+    places_info = {"type": "FeatureCollection", "features": []}
+    for place_id, file_path in files.items():
+        with open(file_path, "r") as file:
+            place = json.load(file)
+            feature = {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        place["coordinates"]["lng"],
+                        place["coordinates"]["lat"],
+                    ],
+                },
+                "properties": {
+                    "title": place["title"],
+                    "placeId": place_id,
+                    "detailsUrl": file_path,
+                },
+            }
+            places_info["features"].append(feature)
+
+    return render(request, "index.html", {"places_info": places_info})
