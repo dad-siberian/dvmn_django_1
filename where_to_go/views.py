9b010e06c63dc9137f4bfa9198961dfc84bcde0c
dad-siberian <dad.siberian@gmail.com>
2024-03-17 19:34:31 +0300

    server returns JSON on Endpoint API request /places/<id>/

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
 
-from places.models import Place
+from places.models import Photo, Place
 
 
 def show_main(request):
@@ -22,3 +23,21 @@ def show_main(request):
         places_info["features"].append(feature)
 
     return render(request, "index.html", {"places_info": places_info})
+
+
+def show_place_detail(request, place_id):
+    place = get_object_or_404(Place, id=place_id)
+    place_info = {
+        "title": place.title,
+        "imgs": [img.photo.url for img in Photo.objects.filter(place=place)],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lat": place.lat,
+            "lng": place.lng,
+        },
+    }
+
+    return JsonResponse(
+        data=place_info, json_dumps_params={"ensure_ascii": False, "indent": 4}
+    )
