7cce51a45c211c335e6cbb230c6fa38feedc98c8
dad-siberian <dad.siberian@gmail.com>
2024-03-17 19:49:01 +0300

    simplifies adding photos to the Place model using Inlines

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import Place, Photo
+
+from .models import Photo, Place
 
 admin.site.register(Photo)
+
+
+class PhotoInline(admin.TabularInline):
+    model = Photo
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ("title",)
+    inlines = [PhotoInline]
