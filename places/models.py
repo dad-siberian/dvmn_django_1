d31c04608448f69443d503c14a31b54c5c814999
dad-siberian <dad.siberian@gmail.com>
2024-03-24 17:39:35 +0300

    makes fields required (8/20)

--- places/models.py
+++ places/models.py
@@ -8,12 +8,8 @@ class Place(models.Model):
 
     """
 
-    title = models.CharField(
-        max_length=200, blank=True, verbose_name="Название места"
-    )
-    short_description = models.TextField(
-        blank=True, verbose_name="Короткое описание"
-    )
+    title = models.CharField(max_length=200, verbose_name="Название места")
+    short_description = models.TextField(verbose_name="Короткое описание")
     long_description = HTMLField(blank=True, verbose_name="Полное описание")
     lng = models.FloatField(verbose_name="Долгота в градусах")
     lat = models.FloatField(verbose_name="Широта в градусах")
@@ -37,10 +33,10 @@ class Photo(models.Model):
         related_name="photos",
         verbose_name="Место",
     )
-    photo = models.ImageField(
-        upload_to="places", null=True, blank=True, verbose_name="Фото"
+    photo = models.ImageField(upload_to="places", verbose_name="Фото")
+    position = models.IntegerField(
+        default=0, blank=True, verbose_name="Позиция"
     )
-    position = models.IntegerField(default=0, verbose_name="Позиция")
 
     class Meta:
         ordering = ["position"]
